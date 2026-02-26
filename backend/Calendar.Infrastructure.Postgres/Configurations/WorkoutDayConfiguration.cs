77e4ced628654a5df7d3f5bdc5b7e2e2c1c7a202
ivnvch <tarasovets.dmtiv@gmail.com>
2026-02-26 17:41:29 +0300

    feat: added configuration

---
+++ backend/Calendar.Infrastructure.Postgres/Configurations/WorkoutDayConfiguration.cs
@@ -0,0 +1,32 @@
+using Calendar.Domain.Calendars.Aggregates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Calendar.Infrastructure.Postgres.Configurations;
+
+public class WorkoutDayConfiguration : IEntityTypeConfiguration<WorkoutDay>
+{
+    public void Configure(EntityTypeBuilder<WorkoutDay> builder)
+    {
+        builder.ToTable("workout_days");
+        
+        builder.HasKey(x => x.Id)
+            .HasName("pk_workout_days_id");
+        
+        builder.HasIndex(x => x.Date)
+            .IsUnique()
+            .HasName("idx_workout_days_date");
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id");
+        
+        builder.Property(x => x.Date)
+            .HasColumnName("date")
+            .IsRequired();
+        
+        builder.HasMany(w  => w.Exercises)
+            .WithOne()
+            .HasForeignKey("workout_day_id")
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
